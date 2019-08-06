07040199375f63f1af82f6aa289129980ff157a2
wangwenkai660 <1039370389@qq.com>
2019-08-06 21:02:52 +0800

    刘红霞的修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("王文凯的新增");
+        System.out.println("刘红霞的新增");
 
     }
 }
